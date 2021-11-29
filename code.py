a527d4960d79405b9ebe9d7a10ba70e9f45c0113
imthimun <imthimun@student.wethinkcode.co.za>
2021-11-29 22:59:18 +0200

    added many lines

--- code.py
+++ code.py
@@ -4,3 +4,7 @@ if __name__ == "__main__":
     print("Fede Bozza")
     print("Fede Bozza")
     print("Fede Bozza")
+    print("Fede Bozza")
+    print("Fede Bozza")
+    print("Fede Bozza")
+    print("Fede Bozza")
