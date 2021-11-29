abb3c742a8a5cd6af9117cd493a8123db3dae694
imthimun <imthimun@student.wethinkcode.co.za>
2021-11-29 23:02:33 +0200

    added lines

--- Code.java
+++ Code.java
@@ -2,5 +2,7 @@ public class Code{
     public static void  main(String[] args){
         System.out.println("Fede Boza!");
         System.out.println("Fede Boza!");
+        System.out.println("Fede Boza!");
+        System.out.println("Fede Boza!");
     }
 }
